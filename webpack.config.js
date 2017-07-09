352166071f805278aa04889a9f0f7389606dce2b
Arsalan Mahmoodpour <Arsalan.mahmoodpour@gmail.com>
2017-07-09 12:09:28 +0430

    webpack config - stylus support - liveScript sopport (using https://github.com/HosseinAgha/react-livescript-boilerplate)

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,2 @@
-module.exports = {
-  entry: './app/app.jsx',
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  resolve: {
-    root: __dirname,
-    alias: {
-    },
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      }
-    ]
-  }
-};
+require('livescript');
+module.exports = require('./webpack.config.ls');
